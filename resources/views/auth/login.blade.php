eb7e1d176e2ecdb9f0f67ad428d7bb0882091b24
christian infurna <christian_infurna_95@hotmail.it>
2023-07-10 23:04:30 +0200

    Front End Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
-    <div>
-        <h2>Accedi</h2>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-6">
+    <div class="container d-flex flex-column align-items-center justify-content-around login-card">
+        <div class="row h-25 align-items-center">
+            <h2>Accedi</h2>
+        </div>
+        <div class="row justify-content-center align-content-evenly w-100 h-75">
+            <div class="col-12 col-md-6 w-100">
                 @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -20,11 +20,11 @@
                         <label for="email" class="form-label">Email address</label>
                         <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 row justify-content-center">
                         <label for="password" class="form-label">Password</label>
                         <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                        <button class="my-5 w-25" type="submit" class="btn btn-primary">Accedi</button>
                     </div>
-                    <button type="submit" class="btn btn-primary">Accedi</button>
                 </form>
             </div>
         </div>
