46807535b7ab735a8e4aafe1af6610bc55d58005
unknown <loris.foglia@gmail.com>
2023-07-10 15:30:28 +0200

    navbar e annunci

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>    
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+<body>
+    @if(session('message'))
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        {{session('message')}}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
+    <x-navbar/>
+
+    {{$slot}}
+    
+    <x-footer/>
+    @livewireScripts
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+
+<h2>fghjkl</h2>
+
+</x-layout>
