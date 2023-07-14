317a059a5ef64142779a69051739cfff8a833023
christian infurna <christian_infurna_95@hotmail.it>
2023-07-14 11:24:22 +0200

    Aggiornamento Front-End

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
-<header class="container-fluid position-relative d-flex align-items-center">
-    <div class="container header-custom rounded-pill d-flex flex-column justify-content-center">
+<div class="container vh-100 w-75 my-5">
+    <header class="container header-custom rounded-pill d-flex flex-column justify-content-center">
         <section>
             <article class="row w-100 align-items-center justify-content-center">
                 <section class="col-12 col-md-6 h-75 d-flex flex-column align-items-center">
@@ -14,16 +14,70 @@
                 </section>
             </article>
         </section>
-        <div class="d-flex justify-content-center align-items-center h-25">
-            <div>
+        <section class="d-flex justify-content-center align-items-center h-25">
+            <article>
                 <button class="button-82-pushable" role="button">
                     <span class="button-82-shadow"></span>
                     <span class="button-82-edge"></span>
                     <span class="button-82-front text">
-                      Esplora
+                        Esplora
                     </span>
                 </button>
+            </article>
+        </section>
+    </header>
+
+    <div class="container headerCustom">
+        <div class="row my-4">
+            <div class="col-12 col-md-6 d-flex justify-content-around">
+                <div class="d-flex flex-column align-items-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                    <h3>Motori</h3>
+                    <a href="{{ route('categoryShow', ['category' => 1]) }}"><i class="fa-solid fa-car fa-xl" style="color: #000000;"></i></a>
+                </div>
+    
+                <div class="d-flex flex-column align-items-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                    <h3>Abbigliamento</h3>
+                    <a href="{{ route('categoryShow', ['category' => 2]) }}"><i class="fa-sharp fa-solid fa-xl fa-shirt" style="color: #000000;"></i></a>
+                </div>
+            </div>
+    
+            <div class="col-12 col-md-6 d-flex justify-content-around">
+                <div class="d-flex flex-column align-items-center" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                    <h3>Arredamento</h3>
+                    <a href="{{ route('categoryShow', ['category' => 3]) }}"><i class="fa-solid fa-couch" style="color: #000000;"></i></a>
+                </div>
+    
+                <div class="d-flex flex-column align-items-center" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                    <h3>Tecnologia</h3>
+                    <a href="{{ route('categoryShow', ['category' => 4]) }}"><i class="fa-solid fa-xl fa-laptop" style="color: #000000;"></i></a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row my-4">
+            <div class="col-12 col-md-6 d-flex justify-content-around">
+                <div class="d-flex flex-column align-items-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                    <h3>Musica</h3>
+                    <a href="{{ route('categoryShow', ['category' => 5]) }}"><i class="fa-solid fa-xl fa-music" style="color: #000000;"></i></a>
+                </div>
+    
+                <div class="d-flex flex-column align-items-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                    <h3>Attrezzatura</h3>
+                    <a href="{{ route('categoryShow', ['category' => 6]) }}"><i class="fa-solid fa-xl fa-toolbox" style="color: #000000;"></i></a>
+                </div>
+            </div>
+    
+            <div class="col-12 col-md-6 d-flex justify-content-around">
+                <div class="d-flex flex-column align-items-center" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                    <h3>Sport</h3>
+                    <a href="{{ route('categoryShow', ['category' => 7]) }}"><i class="fa-solid fa-futbol fa-xl" style="color: #000000;"></i></a>
+                </div>
+    
+                <div class="d-flex flex-column align-items-center" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                    <h3>Film</h3>
+                    <a href="{{ route('categoryShow', ['category' => 8]) }}"><i class="fa-solid fa-clapperboard fa-xl" style="color: #000000;"></i></a>
+                </div>
             </div>
         </div>
     </div>
-</header>
+</div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,32 +3,47 @@
     <a class="navbar-brand fs-2 fw-2 col-1 d-flex align-items-center" href="/">
       <img class="img-navbar" src="./img/arrow.png" alt="">
     </a>
-    <form action="{{route('item.search')}}" class="d-flex" role="search">
-      <input name="searched" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler order-last ms-2 me-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center w-100">
+    </button>    
+    <form action="{{route('item.search')}}" class="d-flex col-md-3 col-6" role="search">
+      <input name="searched" class="form-control input-custom me-2" type="search" placeholder="Ricerca..." aria-label="Search">
+      <button class="button-82-pushable" type="submit" role="button">
+        <span class="button-82-shadow"></span>
+        <span class="button-82-edge"></span>
+        <span class="button-82-front text">
+          Cerca
+        </span>
+      </button>
+    </form>
+    <div class="collapse navbar-collapse col-md-3" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center align-items-center w-100">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="/">Home</a>
         </li>
         @if(Auth::user() != null)
-          <li class="nav-item">
+        <li class="nav-item dropdown ">
+          <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Miei Annunci
+          </a>
+          <ul class="dropdown-menu"  aria-labelledby="categoriesDropdown">
+            <li class="nav-item">
               <a class="nav-link" aria-current="page" href="{{route('item.create')}}">Aggiungi prodotto</a>
-          </li>        
+            </li> 
+            @auth
+            @if(Auth::user()->is_revisor)
+            <li>
+                <a class="nav-link" aria-current="page" href="{{route('revisor.index')}}">Zona Revisore
+                <span class="" >{{App\Models\Item::toBeRevisionedCount()}} <span class="visually-hidden">unread messages</span></span>
+                </a>
+            </li>        
+            @endif 
+            @endauth 
+          </ul>
+        </li>
+                
         @endif
-        @auth
-        @if(Auth::user()->is_revisor)
-          <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="{{route('revisor.index')}}">Zona Revisore
-              <span class="" >{{App\Models\Item::toBeRevisionedCount()}} <span class="visually-hidden">unread messages</span></span>
-              </a>
-          </li>        
-        @endif 
-        @endauth   
+           
         <li class="nav-item dropdown ">
           <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Categorie
@@ -45,29 +60,33 @@
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="{{route('item.index')}}">Tutti gli Annunci</a>
         </li>
-      </ul>
-        @if(Auth::user() != null)
-        <ul class="ps-0 row align-items-center my-0 me-4">
-          <li class="nav-item col-12 col-md-6 px-0 pt-2">
-            <p class="nav-link">{{Auth::user()->name}}</p>
-          </li>
-          <li class="nav-item col-12 col-md-6 px-3">
-            <form action="{{route('logout')}}" method="post" class="nav-link">
-              @csrf
-              <button type="submit"><i class="fa-solid fa-right-from-bracket fa-xl"></i></button>
-            </form>
-          </li>
+        <div class="collapse navbar-collapse col-md-1 order-md-last mx-3 d-flex justify-content-end">
         </ul>
-        @else
-        <ul class="ps-0 row align-items-center my-0">
-          <li class="nav-item col-12 col-md-6 pe-4">
-            <a class="nav-link" aria-current="page" href="{{route('register')}}">Registrati</a>
-          </li>
-          <li class="nav-item col-12 col-md-6 pe-4">
-            <a class="nav-link" aria-current="page" href="{{route('login')}}">Accedi</a>
-          </li>
+          @if(Auth::user() != null)
+          <ul class="ps-0 row align-items-center my-0 col-md-5 justify-content-end">
+            <li class="nav-item col-12 col-md-5 px-0 pt-2 d-flex align-items-center text-center">
+              <p class="nav-link">{{Auth::user()->name}}</p>
+            </li>
+            <li class="nav-item col-12 col-md-5 px-3 d-flex justify-content-start">
+              <form action="{{route('logout')}}" method="post" class="nav-link ">
+                @csrf
+                <button class="d-flex justify-content-center align-items-center button-custom" type="submit"><i class="fa-solid fa-right-from-bracket fa-xl"></i></button>
+              </form>
+            </li>
+          </ul>
+          @else
+          <div class="order-md-last">
+            <ul class="ps-0 row align-items-center my-0 me-3">
+              <li class="nav-item col-12 col-md-12 col-lg-6 pe-4">
+                <a class="nav-link" aria-current="page" href="{{route('register')}}">Registrati</a>
+              </li>
+              <li class="nav-item col-12 col-md-12 col-lg-6 pe-4">
+                <a class="nav-link" aria-current="page" href="{{route('login')}}">Accedi</a>
+              </li>
+            </ul>
+          </div>
+          @endif
         </ul>
-        @endif
     </div>
   </div>
 </nav>
