eb3bc1a208969afb4b2396d0892ce77e073aaa28
christian infurna <christian_infurna_95@hotmail.it>
2023-07-18 22:55:36 +0200

    FrontEnd Aggiornato

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,26 +1,52 @@
-<nav class="navbar navbar-expand-lg nav-custom container-fluid">
-  <div class="container-fluid py-2">
-    <div class="row justify-content-between">
-    <a class="navbar-brand col-2 d-flex align-items-center" href="/">
-      <img class="img-fluid" src="/storage/img/arrow.png" width="80" height="90" alt="">
+<nav class="navbar navbar-expand-lg header5 container-fluid">
+    <a class="ms-2" href="/">
+      <img class="img-fluid" src="/storage/img/arrow.png" width="50" height="60" alt="">
     </a>
-    <button class="navbar-toggler order-last me-2 col-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon "></span>
-    </button>    
-    <form action="{{route('item.search')}}" class="d-flex col-md-7 col-6 mx-2" role="search">
-      <input name="searched" class="form-control input-custom me-2" type="search" placeholder="{{__('ui.searchbox')}}
-" aria-label="Search">
-      <button class="button-82-pushable" type="submit" role="button">
-        <span class="button-82-shadow"></span>
-        <span class="button-82-edge"></span>
-        <span class="button-82-front text">
-          <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>
-        </span>
+    <div class="col-11 mx-auto d-flex align-items-center justify-content-between order-1 w-50 me-2">
+      <div class="searchBox">
+        <input class="searchInput" type="text" name="" placeholder="Search">
+        <button class="searchButton" href="#">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon "></span>
       </button>
-    </form>
-  </div>
-    <div class="collapse navbar-collapse col-md-8" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center align-items-center w-100">
+    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center align-items-center w-100 col-12 col-md-10 order-1">
+        <li class="nav-item col-12 col-md-1 d-flex justify-content-center">
+          <a class="nav-link text-center" aria-current="page" href="/">Home</a>
+        </li>
+      </ul>
+    </div>
+  {{-- <ul class="menu5">
+    <li>Home</li>
+    <li>About</li>
+    <li>Blog</li>
+    <li>Projects</li>
+    <li>Contact</li>
+  </ul> --}}
+</nav>
+
+
+
+{{-- <nav class="navbar navbar-expand-lg nav-custom container-fluid"> --}}
+  {{-- <div class="container-fluid py-2"> --}}
+    {{-- <div class="row justify-content-between">  --}}
+      {{-- <form action="{{route('item.search')}}" class="d-flex col-md-7 col-6 mx-2" role="search">
+        <input name="searched" class="form-control input-custom me-2" type="search" placeholder="{{__('ui.searchbox')}}" aria-label="Search">
+        <button class="button-82-pushable" type="submit" role="button">
+          <span class="button-82-shadow"></span>
+          <span class="button-82-edge"></span>
+          <span class="button-82-front text">
+            <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>
+          </span>
+        </button>
+      </form> --}}
+    {{-- </div> --}}
+    {{-- <div class="collapse navbar-collapse col-md-8" id="navbarSupportedContent"> --}}
+      {{-- <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center align-items-center w-100">
         <li class="nav-item col-12 col-md-1 d-flex justify-content-center">
           <a class="nav-link text-center" aria-current="page" href="/">Home</a>
         </li>
@@ -34,31 +60,29 @@
           <x-_locale lang='es' />
         </li>
         @if(Auth::user() != null)
-        <li class="nav-item dropdown col-12 col-md-3 d-flex flex-column align-items-center justify-content-center">
-          <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          {{__('ui.myAdvertisements')}}
-          </a>
-          <ul class="dropdown-menu"  aria-labelledby="categoriesDropdown">
-            <li class="nav-item col-12 text-center">
-              <a class="nav-link" aria-current="page" href="{{route('item.create')}}">{{__('ui.sellItemNav')}}</a>
-            </li> 
-            @auth
-            @if(Auth::user()->is_revisor)
-            <li class="nav-item col-12 text-center">
-                <a class="nav-link" aria-current="page" href="{{route('revisor.index')}}">{{__('ui.reviewerArea')}}
-                <span class="" >{{App\Models\Item::toBeRevisionedCount()}} <span class="visually-hidden">unread messages</span></span>
-                </a>
-            </li>        
-            @endif 
-            @endauth 
-          </ul>
-        </li>
-                
+          <li class="nav-item dropdown col-12 col-md-3 d-flex flex-column align-items-center justify-content-center">
+            <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              {{__('ui.myAdvertisements')}}
+            </a>
+            <ul class="dropdown-menu"  aria-labelledby="categoriesDropdown">
+              <li class="nav-item col-12 text-center">
+                <a class="nav-link" aria-current="page" href="{{route('item.create')}}">{{__('ui.sellItemNav')}}</a>
+              </li> 
+              @auth
+              @if(Auth::user()->is_revisor)
+              <li class="nav-item col-12 text-center">
+                  <a class="nav-link" aria-current="page" href="{{route('revisor.index')}}">{{__('ui.reviewerArea')}}
+                  <span class="" >{{App\Models\Item::toBeRevisionedCount()}} <span class="visually-hidden">unread messages</span></span>
+                  </a>
+              </li>        
+              @endif 
+              @endauth 
+            </ul>
+          </li>        
         @endif
-           
         <li class="nav-item dropdown col-md-3 col-12 d-flex flex-column justify-content-center align-items-center">
           <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          {{__('ui.categories')}}
+            {{__('ui.categories')}}
           </a>
           <ul class="dropdown-menu"  aria-labelledby="categoriesDropdown">
             @foreach ($categories as $category)
@@ -74,24 +98,19 @@
         </li>
         <div class="collapse navbar-collapse col-md-1 order-md-last mx-3 d-flex justify-content-end">
         </ul>
-          @if(Auth::user() != null)
-            <ul class="ps-0 row align-items-center my-0 col-md-5 justify-content-end">
-            
+        @if(Auth::user() != null)
+          <ul class="ps-0 row align-items-center my-0 col-md-5 justify-content-end">    
             <li class="nav-item col-6 col-md-5 px-0 d-flex align-items-center text-center justify-content-end">
               <a href="{{ route('user.profile') }}"><i class="fa-solid fs-4 fa-user fa-xl ms-3 " style="color: #f0003c;"></i></a>
-            </li>
-          
+            </li>  
             <li class="nav-item col-6 col-md-5 px-3 d-flex justify-content-start">
-            <form action="{{route('logout')}}" method="post" class="nav-link ">
-              @csrf
-              <button class="justify-content-center align-items-center button-logout" type="submit"><i class="fa-solid fa-sign-out fa-xl"></i></button>
-            </form>
-          </li>
-        </ul>
-
-            </div>
-          
-          @else
+              <form action="{{route('logout')}}" method="post" class="nav-link ">
+                @csrf
+                <button class="justify-content-center align-items-center button-logout" type="submit"><i class="fa-solid fa-sign-out fa-xl"></i></button>
+              </form>
+            </li>
+          </ul>
+        @else
           <div class="order-md-last">
             <ul class="ps-0 row align-items-center my-0 me-3">
               <li class="nav-item col-12 col-md-12 col-lg-6 pe-4">
@@ -102,10 +121,10 @@
               </li>
             </ul>
           </div>
-          @endif
-        </ul>
-    </div>
-</nav>
+        @endif
+      </ul> --}}
+    {{-- </div> --}}
+{{-- </nav> --}}
 
 @if(session('message'))
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
