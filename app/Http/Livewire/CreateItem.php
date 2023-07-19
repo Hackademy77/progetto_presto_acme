46e5aa9106be9ea3edbd0efcad08d58cf3970486
Emilio Junior Corona <you@example.com>
2023-07-19 18:07:02 +0200

    User story 8 terminata

--- app/Http/Livewire/CreateItem.php
+++ app/Http/Livewire/CreateItem.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Item;
 use Livewire\Component;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use App\Jobs\GoogleVisionLabelImage;
@@ -88,9 +89,12 @@ class CreateItem extends Component
                 $newFileName = "items/{$this->item->id}";
                 $newImage = $this->item->images()->create(['path'=>$image->store($newFileName, 'public')]);
 
-                dispatch(new ResizeImage($newImage->path ,300 ,200));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path ,300 ,200),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                ])->dispatch($newImage->id);
+                
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
